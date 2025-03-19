2b2ec3d300ab71304b05bca481b319250fa5396f
Programmer-Rakesh <rakeshaps8866@gmail.com>
2025-03-19 23:33:39 +0530

    New file

---
+++ part 2/MainClass.java
@@ -0,0 +1,16 @@
+// package Java.part 2;
+
+// We can call function which is not in the main function
+
+public class MainClass {
+    public static void main(string[] args) {
+        sayHi();
+    }
+
+    public static void sayHi
+    {
+    }
+    {
+        system.out.println("Hi BRUHH");
+    }
+}

18a57908af7874359e253fecda9d72ced1fade7a
Programmer-Rakesh <rakeshaps8866@gmail.com>
2025-03-25 22:45:53 +0530

    New Learnings

--- part 2/Bin.java
+++ part 2/Bin.java
@@ -1,3 +1,19 @@
+// Running a diff function ousdie the main function
+
 public class Bin {
     
+    public static void main(String[] args){
+        System.out.println("Before say hii");
+        sayHi();
+        System.out.println("After say hii");
+        sayHii();
+    }
+
+    public static void sayHi(){
+        System.out.println("Hi This is Rakesh");
+    }
+
+    private static void sayHii(){
+        System.out.println("Private one");
+    }
 }
---
+++ part 2/Main.java
@@ -0,0 +1,7 @@
+// We can run the function from diff file by calling the function...But it should be public
+
+public class Main { 
+    public static void main(String[] args){
+        Bin.sayHi();   // It can be used bcoz sayHi function was public
+    }
+}
